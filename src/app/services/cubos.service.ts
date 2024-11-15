5f5c9cab8bf23cab7ec01a0b8a56af5b17eeca45
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2024-11-15 11:32:16 +0100

    change

--- src/app/services/cubos.service.ts
+++ src/app/services/cubos.service.ts
@@ -84,24 +84,4 @@ export class CubosService {
 
     return this.http.post(this.apiCubos + request, {}, { headers })
   }
-
-  // getAlumnos(): Observable<any> {
-
-  //   const token = this.getToken();
-  //   const headers = new HttpHeaders({
-  //     'Authorization': `Bearer ${token}`
-  //   });
-
-  //   return this.http.get('https://apiejemplos.azurewebsites.net/api/Alumnos/AlumnosToken', { headers })
-  // }
-
-  // postAlumno(alumno: Alumno): Observable<any> {
-
-  //   const token = this.getToken();
-  //   const headers = new HttpHeaders({
-  //     'Authorization': `Bearer ${token}`,
-  //     'Content-Type': 'application/json'
-  //   });
-  //   return this.http.post('https://apiejemplos.azurewebsites.net/api/Alumnos/InsertAlumnoToken', JSON.stringify(alumno), { headers })
-  // }
 }
